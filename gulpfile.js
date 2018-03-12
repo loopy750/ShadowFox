a6e5590fe814eef02a4388a53a7247cee265778f
loopy750 <35911135+loopy750@users.noreply.github.com>
2018-03-12 12:56:49 +1100

    Clean up code, Firefox 59 ready

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ var concatCss = require('gulp-concat-css');
 
 /* Add everything to userChrome */
 gulp.task('userChrome_all', function() {
-  return gulp.src(['color_variables.css', 'userChrome-files/*.css'])
+  return gulp.src(['css/color_variables.css', 'css/userChrome-files/*.css'])
     .pipe(concatCss('userChrome.css'))
     .pipe(gulp.dest('.'));
 });
 
 /* Add everything to userContent */
 gulp.task('userContent_all', function() {
-  return gulp.src(['color_variables.css', 'userContent-files/all_about_pages.css', 'userContent-files/*/*.css'])
+  return gulp.src(['css/color_variables.css', 'css/userContent-files/all_about_pages.css', 'css/userContent-files/*/*.css'])
     .pipe(concatCss('userContent.css'))
     .pipe(gulp.dest('.'));
 });
